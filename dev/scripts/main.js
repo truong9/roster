e4c4e775045dc9da65c7e3b57693b723c3ee3777
Tony Truong <92truong@gmail.com>
2017-07-24 22:52:00 -0400

    added data-id and some functionality

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,31 +1,37 @@
-$(`.team`).on('click', function(e){
-	$('.roster').css('display', 'block');
-	var number = $(this).index() + 1;
-	$('.banner').css('background-image', `url(../assets/${number}.jpg)`);
-})
-
-$('.roster').on('click', function(){
-	$('.roster').css('display', 'none');
-	$('.roster img').remove();
-})
-
-var apiUrlTeam = '';
+var teams = {};
+var teamId = {};
 var apiUrl = 'https://api.mysportsfeeds.com/v1.1/sample/pull/nba/2016-2017-regular/cumulative_player_stats.json?';
-
 
 $.ajax ({
 	url: `${apiUrl}`,
 	method: 'get',
 	dataType: 'json'
 }).then(function(res){
-	var teams = res.cumulativeplayerstats.playerstatsentry;
-	console.log(teams)
+	teams = res.cumulativeplayerstats.playerstatsentry;
+});
+
+$(`.team`).on('click', function(e){
+	$('.roster').css('display', 'block');
+	const number = $(this).index() + 1;
+	$('.banner').css('background-image', `url(../assets/${number}.jpg)`);
+	teamId = $(this).data('id');
+
+	console.log(teamId);
 	
-	// var teams = players.map(function(i){
-	// 	return 
+	// teams.map(player => {
+	// 	if(player.team.Name === teamId){
+	// 		console.log(teamId);
+	// 	}
 	// })
 
 });
+
+$('.roster').on('click', function(){
+	$('.roster').css('display', 'none');
+	$('.roster img').remove();
+})
+
+
 
 
 
@@ -34,3 +40,9 @@ $.ajax ({
 $(function(){
 
 });
+
+	// teams.map(player => {
+	// 	if(player.team.Name === {
+	// 		console.log(this.data("id"))
+	// 	}
+	// });
