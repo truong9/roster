5cdd3cd275c33a2e31c1b6b4ba4f2ea297e85ab8
Tony Truong <92truong@gmail.com>
2017-07-25 16:52:25 -0400

    added players to roster dynamically

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -8,41 +8,32 @@ $.ajax ({
 	dataType: 'json'
 }).then(function(res){
 	teams = res.cumulativeplayerstats.playerstatsentry;
+	console.log(teams)
+
+	$(`.team`).on('click', function(e){
+		$('.roster').css('display', 'block');
+		const number = $(this).index() + 1;
+		$('.banner').css('background-image', `url(../assets/${number}.jpg)`);
+		teamId = $(this).data('id');
+		console.log(teamId);
+
+		teams.map(player => {
+			if(player.team.Name === teamId){
+				$('.roster .players ul').append(`<li>${player.player.FirstName}</li>`)
+			}
+		})
+	});
+
+	$('.roster').on('click', function(){
+		$('.roster').css('display', 'none');
+		$('.roster img').remove();
+		$('.roster .players ul').empty();
+	})
+
 });
-
-$(`.team`).on('click', function(e){
-	$('.roster').css('display', 'block');
-	const number = $(this).index() + 1;
-	$('.banner').css('background-image', `url(../assets/${number}.jpg)`);
-	teamId = $(this).data('id');
-
-	console.log(teamId);
-	
-	// teams.map(player => {
-	// 	if(player.team.Name === teamId){
-	// 		console.log(teamId);
-	// 	}
-	// })
-
-});
-
-$('.roster').on('click', function(){
-	$('.roster').css('display', 'none');
-	$('.roster img').remove();
-})
-
-
-
-
 
 
 
 $(function(){
 
 });
-
-	// teams.map(player => {
-	// 	if(player.team.Name === {
-	// 		console.log(this.data("id"))
-	// 	}
-	// });
