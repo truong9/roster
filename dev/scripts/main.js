9bea4cd95c0883a15deafddd4f205281197efc6f
Tony Truong <92truong@gmail.com>
2017-08-01 11:15:56 -0400

    finalized responsive

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -2,14 +2,16 @@ var teams = {};
 var teamId = {};
 var apiUrl = 'https://api.mysportsfeeds.com/v1.1/sample/pull/nba/2016-2017-regular/cumulative_player_stats.json?';
 
+// AJAX call to SportsFeed
 $.ajax ({
 	url: `${apiUrl}`,
 	method: 'get',
 	dataType: 'json'
 }).then(function(res){
 	teams = res.cumulativeplayerstats.playerstatsentry;
-	console.log(teams);
+	// console.log(teams);
 
+// View Roster modal based on team clicked
 	$(`.team`).on('click', function(e){
 		$('.roster').css('display', 'block');
 		const number = $(this).index() + 1;
@@ -24,6 +26,7 @@ $.ajax ({
 		})
 	});
 
+// Clear modal once its closed
 	$('.roster').on('click', function(){
 		$('.roster').css('display', 'none');
 		$('.roster img').remove();
@@ -31,24 +34,25 @@ $.ajax ({
 	})
 });
 
+// Music Player only on desktop version
 var music = document.getElementById('music');
 
-	if ($(window).width() > 750){
-		$('i').on('click', function(){
-			if(music.paused == false){
-				music.pause();
-				$('.fa-volume-up').css('visibility', 'hidden');
-				$('.fa-volume-off').css('visibility', 'visible');
-			} else {
-				music.play();
-				$('.fa-volume-up').css('visibility', 'visible');
-				$('.fa-volume-off').css('visibility', 'hidden');
-			}
-		});
-	} else {
-		music.pause();
-		$('i').css('display', 'none');
-	}
+if ($(window).width() > 750){
+	$('i').on('click', function(){
+		if(music.paused == false){
+			music.pause();
+			$('.fa-volume-up').css('visibility', 'hidden');
+			$('.fa-volume-off').css('visibility', 'visible');
+		} else {
+			music.play();
+			$('.fa-volume-up').css('visibility', 'visible');
+			$('.fa-volume-off').css('visibility', 'hidden');
+		}
+	});
+} else {
+	music.pause();
+	$('i').css('display', 'none');
+};
 
 
 
